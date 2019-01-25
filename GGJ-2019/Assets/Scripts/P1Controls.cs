f279bad2a58bd24ff09ca075f5872492d12dae85
Torbjornsson <david.torbjornsson@gmail.com>
2019-01-25 23:05:22 +0100

    adds control for players

---
+++ GGJ-2019/Assets/Scripts/P1Controls.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1Controls : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal1");
+        float moveV = Input.GetAxis("Vertical1");
+
+        Vector3 movement = new Vector3 (moveH, 0.0f, moveV);
+
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ GGJ-2019/Assets/Scripts/P2Controls.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P2Controls : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal2");
+        float moveV = Input.GetAxis("Vertical2");
+
+        Vector3 movement = new Vector3 (moveH, 0.0f, moveV);
+
+        rb.AddForce(movement * speed);
+    }
+}
