a0f7a57657b6a21d842de70fac0e1e51cde140aa
Torbjornsson <david.torbjornsson@gmail.com>
2019-01-26 23:46:48 +0100

    gui

---
+++ GGJ-2019/Assets/Code/Guiscene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guiscene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Panel panel;
+    void Start()
+    {
+        //panel = GetComponent<Panel>();
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
