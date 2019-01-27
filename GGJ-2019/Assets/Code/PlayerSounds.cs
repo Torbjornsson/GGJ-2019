e84631baf1651cb19c4fe56010fdca4ff4f240bc
Andreas Wilcox <wilcox.andreas@gmail.com>
2019-01-27 13:26:02 +0100

    Added a whole bunch of sounds

---
+++ GGJ-2019/Assets/Code/PlayerSounds.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class PlayerSounds : MonoBehaviour
+{
+    public CombinedController Controller;
+    public Grabber Grabber;
+    public LoopSound WalkSound;
+    public AudioSource DragSound;
+    public VariationSounds PickupSound;
+    public VariationSounds DropSound;
+
+    private bool _wasGrabbing;
+
+    public void Update()
+    {
+        UpdateDragSound();
+        UpdateWalkSound();
+        UpdatePickupAndDrop();
+    }
+
+    private void UpdateWalkSound()
+    {
+        var moveDelta = Controller.Velocity.magnitude;
+        if (moveDelta > 1)
+            WalkSound.Play();
+        else
+            WalkSound.Stop();
+    }
+
+    private void UpdateDragSound()
+    {
+        var dragDiff = 0f;
+        var currentlyGrabbedObject = Grabber.CurrentlyGrabbedObject();
+        if (currentlyGrabbedObject != null)
+        {
+            dragDiff = currentlyGrabbedObject.velocity.magnitude*0.75f;
+        }
+        DragSound.volume = Mathf.Min(1, dragDiff);
+
+        if (Grabber.HoldingHeavy && !DragSound.isPlaying)
+        {
+            DragSound.volume = 0;
+            DragSound.Play();
+        }
+
+        if (!Grabber.HoldingHeavy)
+            DragSound.Stop();
+    }
+
+    private void UpdatePickupAndDrop()
+    {
+        if(_wasGrabbing && !Grabber.grabbing)
+            DropSound.Play();
+        if(!_wasGrabbing && Grabber.grabbing)
+            PickupSound.Play();
+
+        _wasGrabbing = Grabber.grabbing;
+    }
+}
